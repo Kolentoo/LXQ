c73212cb2a96ce746ba1b1f6d25eb371b2cf092e
LXQ <515835543@qq.com>
2017-06-29 18:11:01 +0800

    vuejs study about todolist
    Signed-off-by: LXQ <515835543@qq.com>

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <!--<img src="./assets/logo.png">-->
+    <h1>{{title}}</h1>
+    <h2 v-text='title'></h2>
+    <p v-html='content'></p>
+    <ul>
+      <li v-for='item in items' v-bind:class='{finished:item.isFinished}' @click='toggleClick(item)'>
+        {{item.label}}
+      </li>
+    </ul>
+    <input v-model='newItem' v-on:keyup.enter='addNewItem'>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
 
 export default {
-  name: 'app',
-  components: {
-    Hello
+  data() {
+    return{
+      title:'todo list',
+      content:'todo content<span>!</span>',
+      items:[
+        {
+          label:'coding',
+          isFinished:false
+        },
+        {
+          label:'walking',
+          isfinished:true
+        }
+      ],
+      // liClass:'lxq'
+      newItem:''
+    }
+  },
+  methods:{
+    toggleClick:function (item){
+      console.log(item);
+      item.isFinished = !item.isFinished;
+    },
+    addNewItem:function(){
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      });
+      this.newItem='';
+    }
   }
 }
 </script>
@@ -25,4 +60,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.finished {text-decoration: underline;color:blue;}
 </style>
